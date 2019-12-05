c03206c9b087ec8f49ee59915629f61b3bcc8497
Adam Febriansyah <afebriansyah400@gmail.com>
2019-12-05 10:35:48 +0700

    001

--- src/okhtemp/MainSa.java
+++ src/okhtemp/MainSa.java
@@ -50,7 +50,7 @@ public class MainSa{
 
         Scanner in = new Scanner(System.in);
 
-        int timeslot = 0;
+        int iterasi = 0;
 
         System.out.println("=== TIMESLOTING === \n");
         System.out.println("1. Car-f-92");
@@ -73,118 +73,118 @@ public class MainSa{
         
         switch (input) {
             case 1:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime = System.nanoTime();
-                SimulatedAnnealing.run(dir_carf92_stu, dir_carf92_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_carf92_stu, dir_carf92_crs, 100, iterasi);
                 long endTime = System.nanoTime();
                 long totalTime = endTime - startTime;
                 System.out.println("Total waktu : " + (double) totalTime / 1000000000 + " detik");
                 break;
             case 2:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime2 = System.nanoTime();
-                SimulatedAnnealing.run(dir_cars91_stu, dir_cars91_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_cars91_stu, dir_cars91_crs, 100, iterasi);
                 long endTime2 = System.nanoTime();
                 long totalTime2 = endTime2 - startTime2;
                 System.out.println("Total waktu : " + (double) totalTime2 / 1000000000 + " detik");
                 break;
             case 3:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime3 = System.nanoTime();
-                SimulatedAnnealing.run(dir_earf83_stu, dir_earf83_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_earf83_stu, dir_earf83_crs, 100, iterasi);
                 long endTime3 = System.nanoTime();
                 long totalTime3 = endTime3 - startTime3;
                 System.out.println("Total waktu : " + (double) totalTime3 / 1000000000 + " detik");
                 break;
             case 4:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime4 = System.nanoTime();
-                SimulatedAnnealing.run(dir_hecs92_stu, dir_hecs92_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_hecs92_stu, dir_hecs92_crs, 100, iterasi);
                 long endTime4 = System.nanoTime();
                 long totalTime4 = endTime4 - startTime4;
                 System.out.println("Total waktu : " + (double) totalTime4 / 1000000000 + " detik");
                 break;
             case 5:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime5 = System.nanoTime();
-                SimulatedAnnealing.run(dir_kfus93_stu, dir_kfus93_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_kfus93_stu, dir_kfus93_crs, 100, iterasi);
                 long endTime5 = System.nanoTime();
                 long totalTime5 = endTime5 - startTime5;
                 System.out.println("Total waktu : " + (double) totalTime5 / 1000000000 + " detik");
                 break;
             case 6:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime6 = System.nanoTime();
-                SimulatedAnnealing.run(dir_lsef91_stu, dir_lsef91_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_lsef91_stu, dir_lsef91_crs, 100, iterasi);
                 long endTime6 = System.nanoTime();
                 long totalTime6 = endTime6 - startTime6;
                 System.out.println("Total waktu : " + (double) totalTime6 / 1000000000 + " detik");
                 break;
             case 7:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime7 = System.nanoTime();
-                SimulatedAnnealing.run(dir_purs93_stu, dir_purs93_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_purs93_stu, dir_purs93_crs, 100, iterasi);
                 long endTime7 = System.nanoTime();
                 long totalTime7 = endTime7 - startTime7;
                 System.out.println("Total waktu : " + (double) totalTime7 / 1000000000 + " detik");
                 break;
             case 8:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime8 = System.nanoTime();
-                SimulatedAnnealing.run(dir_ryes93_stu, dir_ryes93_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_ryes93_stu, dir_ryes93_crs, 100, iterasi);
                 long endTime8 = System.nanoTime();
                 long totalTime8 = endTime8 - startTime8;
                 System.out.println("Total waktu : " + (double) totalTime8 / 1000000000 + " detik");
                 break;
             case 9:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime9 = System.nanoTime();
-                SimulatedAnnealing.run(dir_staf83_stu, dir_staf83_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_staf83_stu, dir_staf83_crs, 100, iterasi);
                 long endTime9 = System.nanoTime();
                 long totalTime9 = endTime9 - startTime9;
                 System.out.println("Total waktu : " + (double) totalTime9 / 1000000000 + " detik");
                 break;
             case 10:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime10 = System.nanoTime();
-                SimulatedAnnealing.run(dir_tres92_stu, dir_tres92_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_tres92_stu, dir_tres92_crs, 100, iterasi);
                 long endTime10 = System.nanoTime();
                 long totalTime10 = endTime10 - startTime10;
                 System.out.println("Total waktu : " + (double) totalTime10 / 1000000000 + " detik");
                 break;
             case 11:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime11 = System.nanoTime();
-                SimulatedAnnealing.run(dir_utas92_stu, dir_utas92_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_utas92_stu, dir_utas92_crs, 100, iterasi);
                 long endTime11 = System.nanoTime();
                 long totalTime11 = endTime11 - startTime11;
                 System.out.println("Total waktu : " + (double) totalTime11 / 1000000000 + " detik");
                 break;
             case 12:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime12 = System.nanoTime();
-                SimulatedAnnealing.run(dir_utes92_stu, dir_utes92_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_utes92_stu, dir_utes92_crs, 100, iterasi);
                 long endTime12 = System.nanoTime();
                 long totalTime12 = endTime12 - startTime12;
                 System.out.println("Total waktu : " + (double) totalTime12 / 1000000000 + " detik");
                 break;
             case 13:
-                System.out.print("Jumlah Timeslot : ");
-                timeslot = in.nextInt();
+                System.out.print("Jumlah Iterasi : ");
+                iterasi = in.nextInt();
                 long startTime13 = System.nanoTime();
-                SimulatedAnnealing.run(dir_yorf83_stu, dir_yorf83_crs, 100, 1000);
+                SimulatedAnnealing.run(dir_yorf83_stu, dir_yorf83_crs, 100, iterasi);
                 long endTime13 = System.nanoTime();
                 long totalTime13 = endTime13 - startTime13;
                 System.out.println("Total waktu : " + (double) totalTime13 / 1000000000 + " detik");
